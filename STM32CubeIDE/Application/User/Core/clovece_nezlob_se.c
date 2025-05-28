7817715ed90b1071136eb8b11c7671c53aa69fef
AdamZatloukal <adamzatloukal99@gmail.com>
2025-05-28 19:41:01 +0200

    test

--- STM32CubeIDE/Application/User/Core/clovece_nezlob_se.c
+++ STM32CubeIDE/Application/User/Core/clovece_nezlob_se.c
@@ -261,6 +261,8 @@ Player* select_player(uint8_t player){
  * Notes:
  * Position 254 -> START
  * Position 255 -> END
+ * 
+ * Needs to be updated for new rules
  */
 void move_pawn(uint8_t player, uint8_t number){
 	Player* player_struct = select_player(player);
