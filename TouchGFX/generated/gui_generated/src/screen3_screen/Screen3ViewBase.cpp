40eb75864e80a95499494e28a9c1a83fa8d164e3
AdamZatloukal <adamzatloukal99@gmail.com>
2025-06-07 23:01:26 +0200

    Opravena chyba https://github.com/AdamZatloukal/DMP/issues/6

--- TouchGFX/generated/gui_generated/src/screen3_screen/Screen3ViewBase.cpp
+++ TouchGFX/generated/gui_generated/src/screen3_screen/Screen3ViewBase.cpp
@@ -52,7 +52,7 @@ Screen3ViewBase::Screen3ViewBase()
     diceResultImage6.setVisible(false);
     add(diceResultImage6);
 
-    AIText.setXY(259, 0);
+    AIText.setXY(260, 0);
     AIText.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
     AIText.setLinespacing(0);
     AIText.setTypedText(touchgfx::TypedText(T___SINGLEUSE_IJII));
--- TouchGFX/generated/gui_generated/src/screen4_screen/Screen4ViewBase.cpp
+++ TouchGFX/generated/gui_generated/src/screen4_screen/Screen4ViewBase.cpp
@@ -82,7 +82,7 @@ Screen4ViewBase::Screen4ViewBase()
     diceResultImage6.setVisible(false);
     add(diceResultImage6);
 
-    AIText.setXY(269, 0);
+    AIText.setXY(260, 0);
     AIText.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
     AIText.setLinespacing(0);
     AIText.setTypedText(touchgfx::TypedText(T___SINGLEUSE_QXX9));
